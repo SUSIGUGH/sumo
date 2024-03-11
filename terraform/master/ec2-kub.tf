9f0c1753238261a9c7f609f31bbd0bb7bfde9dd3
susigugh <susigugh@gmail.com>
2024-03-11 03:40:01 +0000

    Removed big files

--- terraform/master/ec2-kub.tf
+++ terraform/master/ec2-kub.tf
@@ -8,18 +8,28 @@ resource "aws_instance" "ec2-kub-master" {
   vpc_security_group_ids = ["sg-0284357dedebf8167"]
   key_name = "ads"
   user_data = <<-EOF
-               sudo yum install docker -y
-                sudo systemctl enable docker
-                sudo systemctl restart docker
-               sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-                sudo systemctl enable kubelet
-                sudo systemctl start kubelet
-                sudo kubeadm init --ignore-preflight-errors=all > /tmp/kubeadm-init.log
-                sudo tail -2 /tmp/kubeadm-init.log > /tmp/kubeadmjoin.sh
-                sudo mkdir -p $HOME/.kube
-                sudo sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-                sudo curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/calico.yaml -O
-                sudo kubectl apply -f calico.yaml
-    EOF
+     #!/bin/bash
+     yum install docker -y
+     systemctl enable docker
+     systemctl restart docker
+     cat <<EOF1 > /etc/yum.repos.d/kubernetes.repo
+     [kubernetes]
+     name=Kubernetes
+     baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+     enabled=1
+     gpgcheck=1
+     gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+     exclude=kubelet kubeadm kubectl
+     EOF1
+     yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+     systemctl enable kubelet
+     systemctl start kubelet
+     kubeadm init --ignore-preflight-errors=all > /tmp/kubeadm-init.log
+     tail -2 /tmp/kubeadm-init.log > /tmp/kubeadmjoin.sh
+     mkdir -p /root/.kube
+     cp -i /etc/kubernetes/admin.conf /root/.kube/config
+     curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/calico.yaml -O
+     kubectl apply -f calico.yaml
+     EOF
 }
 
