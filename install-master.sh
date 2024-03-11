aa726a08b85062d2e48641d3ea612c7ac6dd7f21
susigugh <susigugh@gmail.com>
2024-03-11 00:55:39 +0000

    calico.yaml added

--- install-master.sh
+++ install-master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 option="$1"
-if [ "$option" == "o" ];
+if [ "$option" == "-o" ];
 then
 sudo yum remove docker -y
 sudo yum install docker -y
@@ -22,9 +22,37 @@ sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
 sudo systemctl enable kubelet
 sudo systemctl start kubelet
 sudo kubeadm init --ignore-preflight-errors=all > /tmp/kubeadm-init.log
+sudo tail -2 /tmp/kubeadm-init.log > /tmp/kubeadmjoin.sh
 sudo mkdir -p $HOME/.kube
 sudo sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/calico.yaml -O
+sudo kubectl apply -f calico.yaml
 else
-	echo "No verwrite"
+	which docker
+	if [ $? -eq 0 ];
+	then
+		echo "Docker Already Installed"
+	else
+		sudo yum install docker -y
+		sudo systemctl enable docker
+		sudo systemctl restart docker
+	fi
+	which kubeadm
+	if [ $? -eq 0 ];
+        then
+                echo "Kubenetes Already Installed"
+        else
+		rm -f $HOME/.kube/config
+		sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+		sudo systemctl enable kubelet
+		sudo systemctl start kubelet
+		sudo kubeadm init --ignore-preflight-errors=all > /tmp/kubeadm-init.log
+		sudo tail -2 /tmp/kubeadm-init.log > /tmp/kubeadmjoin.sh
+		sudo mkdir -p $HOME/.kube
+		sudo sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+		sudo curl https://raw.githubusercontent.com/projectcalico/calico/v3.27.0/manifests/calico.yaml -O
+		sudo kubectl apply -f calico.yaml
+        fi
+
 fi
