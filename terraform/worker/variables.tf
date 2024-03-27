ac6569687bd41d960d5ed9d7babe8f6aa511c630
susigugh <susigugh@gmail.com>
2024-03-27 01:51:42 +0000

    Changed region

--- terraform/worker/variables.tf
+++ terraform/worker/variables.tf
@@ -1,16 +1,16 @@
 variable "ec2-kub-ami"{
 type = string
-default = "ami-0c7f9161f8491665f"
+default = "ami-0187337106779cdf8"
 description = "AMI for Kubernetes"
 }
 variable "ec2-kub-vpc"{
 type = string
-default = "vpc-0ddfec4c1c6b011a0"
+default = "vpc-00c9962d6840d8c99"
 description = "VPC for Kubernetes"
 }
 variable "ec2-kub-region"{
 type = string
-default = "us-east-2"
+default = "ap-south-1"
 description = "VPC for Kubernetes"
 }
 
